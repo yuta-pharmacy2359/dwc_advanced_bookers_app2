e7b4bd282264afac384d29ac48b62ade0806a0a2
Yuta <yuta.shogai@gmail.com>
2021-02-01 15:48:01 +0000

    [Finish] 応用課題2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,18 +1,26 @@
 class BooksController < ApplicationController
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
 
   def show
+    @book_new = Book.new
+    @user = current_user
     @book = Book.find(params[:id])
   end
 
   def index
+    @user = current_user
+    @book = Book.new
     @books = Book.all
   end
 
   def create
+    @user = current_user
+    @books = Book.all
     @book = Book.new(book_params)
-    @book.user_id = current_user.id
+    @book.user_id = @user.id
     if @book.save
-      redirect_to book_path(@book), notice: "You have created book successfully."
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
     else
       @books = Book.all
       render 'index'
@@ -23,27 +31,32 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
-
-
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      redirect_to book_path(@book), notice: "You have updated book successfully."
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
     else
-      render "edit"
+      render :edit
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
+  def ensure_correct_user
+    unless Book.find(params[:id]).user.id.to_i == current_user.id
+      redirect_to books_path
+    end
+  end
+
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 
 end
